464e42800777237007ad91239fbc4f811e4eed51
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-04-26 00:39:36 +0000

    proyecto finalizado

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import { useState } from "react";
+
+
+
 
 const TrafficLight = () =>{
+    const [lightOn,setlightOn] = useState("");
+
+    const changeLight = (color)=>{
+        setlightOn(color);
+    }
     return(
         <div className="trafficStop">
             <div className="container d-flex flex-column justify-content-center align-items-center">
                 <div className="trafficStopTop">
 
                 </div>
-                <div className="trafficStopLight row col-12 d-flex flex-column justify-content-center align-items-center">
-                    <div className="col-4 light redLight rounded-circle">
+                <div className={`trafficStopLight row col-12 d-flex flex-column justify-content-center align-items-center`}>
+                    <div className={`col-4 lightBody redLight rounded-circle ${lightOn === "red" ? "light" : ""}`} onClick={()=>changeLight("red")}>
 
                     </div>
-                    <div className="col-4 light yellowLight rounded-circle">
+                    <div className={`col-4 lightBody yellowLight rounded-circle ${lightOn === "yellow" ? "light" : ""}`} onClick={() => changeLight("yellow")}>
 
                     </div>
-                    <div className="col-4 light greenLight rounded-circle">
+                    <div className={`col-4 lightBody greenLight rounded-circle ${lightOn === "green" ? "light" : ""}`} onClick={() => changeLight("green")}>
 
                     </div>
                 </div>
