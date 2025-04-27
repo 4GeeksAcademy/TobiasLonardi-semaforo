e37a20f55388a7480ba0fc3232db013146fc6778
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-04-27 21:14:53 +0000

    add extra light with button

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -14,6 +14,9 @@ const TrafficLight = () =>{
             setlightOn("");
         }
     }
+
+    const [showLight,setShowLight] = useState(false)
+
     return(
         <div className="trafficStop">
             <div className="container d-flex flex-column justify-content-center align-items-center">
@@ -21,15 +24,21 @@ const TrafficLight = () =>{
 
                 </div>
                 <div className={`trafficStopLight row col-12 d-flex flex-column justify-content-center align-items-center`}>
-                    <div className={`col-4 lightBody redLight rounded-circle ${lightOn === "red" ? "light" : ""}`} onClick={()=>changeLight("red")}>
+                    <div className={` lightBody redLight rounded-circle ${lightOn === "red" ? "light" : ""}`} onClick={()=>changeLight("red")}>
 
                     </div>
-                    <div className={`col-4 lightBody yellowLight rounded-circle ${lightOn === "yellow" ? "light" : ""}`} onClick={() => changeLight("yellow")}>
+                    <div className={` lightBody yellowLight rounded-circle ${lightOn === "yellow" ? "light" : ""}`} onClick={() => changeLight("yellow")}>
 
                     </div>
-                    <div className={`col-4 lightBody greenLight rounded-circle ${lightOn === "green" ? "light" : ""}`} onClick={() => changeLight("green")}>
+                    <div className={` lightBody greenLight rounded-circle ${lightOn === "green" ? "light" : ""}`} onClick={() => changeLight("green")}>
 
                     </div>
+                    {showLight &&<div className={` lightBody violetLight rounded-circle ${lightOn === "green" ? "light" : ""}`} onClick={() => changeLight("green")}>
+
+                    </div>}
+                    <button onClick={() => setShowLight(!showLight)}>
+                        {showLight ? "show" : "hide"}
+                    </button>
                 </div>
             </div>
         </div>
